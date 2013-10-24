f8e392f00d10fa1327231b291206b0763675b87e
Ben Brown <benbrown93@gmail.com>
2013-10-25 01:07:27 +1300

    SwipeDetector Commented

--- src/com/se206a3/contactmanager/SwipeDetector.java
+++ src/com/se206a3/contactmanager/SwipeDetector.java
@@ -1,45 +1,71 @@
 package com.se206a3.contactmanager;
 
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-
+/**
+ * 
+ * @author Ben Brown
+ *
+ * Swipe detector for list view items.
+ * Swipe left = delete contact.
+ * Swipe right = edit contact.
+ */
 public class SwipeDetector implements View.OnTouchListener {
 
+	/**
+	 * 
+	 * @author Ben Brown
+	 * Enum that defines swipe types
+	 */
     public static enum Action {
         LR, // Left to Right
         RL, // Right to Left
-        TB, // Top to bottom
-        BT, // Bottom to Top
         None // when no action was detected
     }
 
-    private static final String logTag = "SwipeDetector";
-    private static final int MIN_DISTANCE = 100;
-    private float downX, downY, upX, upY;
+    /**
+     * Min distance moved for a swipe to be registered
+     */
+    private static final int MIN_DISTANCE = 75;
+    
+    /**
+     * Points where finger touches and leaves screen
+     */
+    private float downX, upX;
+    
+    /**
+     * Type of swipe
+     */
     private Action mSwipeDetected = Action.None;
 
+    /**
+     * Returns if swipe is detected
+     * @return boolean (If swipe has occurred)
+     */
     public boolean swipeDetected() {
         return mSwipeDetected != Action.None;
     }
 
+    /**
+     * Returns type of swipe
+     * @return swipe type
+     */
     public Action getAction() {
         return mSwipeDetected;
     }
 
     @Override
+    /**
+     * Method that is called on touch of elements
+     */
     public boolean onTouch(View v, MotionEvent event) {
         switch (event.getAction()) {
-        case MotionEvent.ACTION_DOWN:
+        case MotionEvent.ACTION_DOWN: // If no swipe
             downX = event.getX();
-            downY = event.getY();
             mSwipeDetected = Action.None;
             return false; // allow other events like Click to be processed
-        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_UP: // If swipe
             upX = event.getX();
 
             float deltaX = downX - upX;
@@ -48,12 +74,10 @@ public class SwipeDetector implements View.OnTouchListener {
             if (Math.abs(deltaX) > MIN_DISTANCE) {
                 // left or right
                 if (deltaX < 0) {
-                    Log.i(logTag, "Swipe Left to Right");
                     mSwipeDetected = Action.LR;
                     return false;
                 }
                 if (deltaX > 0) {
-                    Log.i(logTag, "Swipe Right to Left");
                     mSwipeDetected = Action.RL;
                     return false;
                 }
